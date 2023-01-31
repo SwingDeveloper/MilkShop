bd48cd49eb509a95bc6a3fb894b5f9471661efc0
SwingDeveloper <aass65547@gmail.com>
2023-01-31 22:58:08 +0800

    新增菜單飲品

--- MilkShop/Controller/HomeViewController.swift
+++ MilkShop/Controller/HomeViewController.swift
@@ -11,7 +11,7 @@ class HomeViewController: UIViewController, UICollectionViewDelegate, UICollecti
     
     @IBOutlet weak var drinksCollectionView: UICollectionView!
     @IBOutlet weak var bannerCollectionView: UICollectionView!
-    let menu = [Menu.Records]()
+    var menuArray = [Menu.Records]()
     let bannerArray: [UIImage] = {
         var array = [UIImage]()
         for i in 1...3 {
@@ -26,21 +26,53 @@ class HomeViewController: UIViewController, UICollectionViewDelegate, UICollecti
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
         drinksCollectionView.delegate = self
         drinksCollectionView.dataSource = self
+        drinksCollectionView.collectionViewLayout = setupDrinksCollectionView()
         bannerCollectionView.delegate = self
         bannerCollectionView.dataSource = self
         bannerCollectionView.collectionViewLayout = setupBannerCollectionView()
+        fetchData()
+        
 
         Timer.scheduledTimer(timeInterval: 2, target: self, selector: #selector(changeBanner), userInfo: nil, repeats: true)
     }
     
     func setupBannerCollectionView() -> UICollectionViewFlowLayout {
         let layout = UICollectionViewFlowLayout()
-        layout.itemSize = CGSize(width: UIScreen.main.bounds.width, height: 200)
+        layout.itemSize = CGSize(width: UIScreen.main.bounds.width, height: 300)
         layout.scrollDirection = .horizontal
         return layout
+    }
+    
+    func setupDrinksCollectionView() -> UICollectionViewFlowLayout {
+        let layout = UICollectionViewFlowLayout()
+        layout.itemSize = CGSize(width: UIScreen.main.bounds.width/2 - 10, height: 200)
+        return layout
+    }
+    
+    func fetchData() {
+        let urlString = "https://api.airtable.com/v0/appfieMsUwTxzERom/Menu?sort[][field]=type&sort[][direction]=desc"
+        if let url = URL(string: urlString) {
+            var request = URLRequest(url: url)
+            request.setValue("Bearer keykgXb1GRqbLNtpC", forHTTPHeaderField: "Authorization")
+            request.httpMethod = "GET"
+            request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+            URLSession.shared.dataTask(with: request) { data, response, error in
+                let decoder = JSONDecoder()
+                if let data = data {
+                    do {
+                        let menu = try decoder.decode(Menu.self, from: data)
+                        DispatchQueue.main.async {
+                            self.menuArray = menu.records
+                            self.drinksCollectionView.reloadData()
+                        }
+                    } catch  {
+                        print(error)
+                    }
+                }
+            }.resume()
+        }
     }
     
     @objc func changeBanner() {
@@ -71,15 +103,30 @@ class HomeViewController: UIViewController, UICollectionViewDelegate, UICollecti
         if collectionView == bannerCollectionView {
             return bannerArray.count
         } else {
-            return menu.count
+            return menuArray.count
         }
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let bannerCell = collectionView.dequeueReusableCell(withReuseIdentifier: "bannerCell", for: indexPath) as! BannerCollectionViewCell
-        bannerCell.bannerImageView.frame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 200)
-        bannerCell.bannerImageView.image = bannerArray[indexPath.row]
-        return bannerCell
+        if collectionView == drinksCollectionView {
+            let drinksCell = collectionView.dequeueReusableCell(withReuseIdentifier: "drinksCell", for: indexPath) as! DrinksCollectionViewCell
+            drinksCell.drinksLabel.text = menuArray[indexPath.row].fields.name
+            let imageURL = URL(string: "\(menuArray[indexPath.row].fields.imageURL)")!
+            URLSession.shared.dataTask(with: imageURL) { data, response, error in
+                if let data = data {
+                    DispatchQueue.main.async {
+                        drinksCell.drinksImageView.image = UIImage(data: data)
+                    }
+                }
+            }.resume()
+            return drinksCell
+            
+        } else {
+            let bannerCell = collectionView.dequeueReusableCell(withReuseIdentifier: "bannerCell", for: indexPath) as! BannerCollectionViewCell
+            bannerCell.bannerImageView.frame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 200)
+            bannerCell.bannerImageView.image = bannerArray[indexPath.row]
+            return bannerCell
+        }
     }
     
 }
---
+++ MilkShop/View/DrinksCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  DrinksCollectionViewCell.swift
+//  MilkShop
+//
+//  Created by macbook_air_1 on 2023/1/31.
+//
+
+import UIKit
+
+class DrinksCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var drinksImageView: UIImageView!
+    @IBOutlet weak var drinksLabel: UILabel!
+}
